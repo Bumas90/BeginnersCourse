cc3a7551ffa0744d9d67ffcc6f2ebf8a2c5f7bcf
Bumas90 <github@stefanwittwer.de>
2022-01-14 15:03:31 +0100

    initial version phpMyAdmin and database cluster

---
+++ Exercises/TerraformAzureApplication/variables.tf
@@ -0,0 +1,30 @@
+
+variable "account_location" {
+  default = "westeurope"
+}
+
+variable "resource_group_name" {
+  default = "nginx-resources"
+}
+
+variable "nginx_hostname" {
+  default = "swit165az303"
+}
+
+variable "nginx_fqdn" {
+  default = "swit165az303.westeurope.cloudapp.azure.com"
+}
+
+variable "mysqlsrv_dns_name" {
+  default = "az304-mysql-srv.mysql.database.azure.com"
+}
+
+variable "mysqlsrv_usr_name" {
+  default = "sqladmin"
+}
+
+variable "mysqlsrv_usr_pwd" {
+  default = "JubjubBird1806!"
+}
+
+
